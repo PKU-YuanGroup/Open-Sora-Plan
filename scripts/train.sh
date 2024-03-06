70e082068a74b569161a3e61faf7a36f92dad1dd
lb203 <62638829+LinB203@users.noreply.github.com>
2024-03-07 00:57:14 +0800

    Update train.sh
    Former-commit-id: 81585b54e2fb5eebda06319f7e1405031db30c69 [formerly cf49f300fbc27249ab18b60d7224a0d593f1378f]
    Former-commit-id: 3d0d91edf49454b8fd1c48afae97bba6b8e6630d

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,7 +1,7 @@
 accelerate launch --multi_gpu --num_processes 7 --mixed_precision bf16 opensora/train/train.py \
-  --model Latte-XL/122 --dataset landscope_feature \
+  --model Latte-XL/122 --dataset sky \
   --ae ucf101_stride4x4x4 \
-  --data-path "/remote-home/yeyang/Open-Sora-Plan/features/*/*npy" --extras 1 \
+  --data-path "/remote-home/yeyang/sky_timelapse/sky_train/" --extras 1 \
   --sample-rate 3 --num-frames 128 --max-image-size 256 \
   --max-train-steps 1000000 --local-batch-size 5 --lr 1e-4 \
   --ckpt-every 500 --log-every 50 --gradient-checkpointing --mixed-precision
