af338d5b498d80673cb53443b9cacb4be6a3ae1c
lb203 <62638829+LinB203@users.noreply.github.com>
2024-03-07 01:06:20 +0800

    Update train.sh
    Former-commit-id: e56090f847a57f52231555612a4f238b59fb707a [formerly f1f7fe1ca1012ea045f7a84bfe39175894f7aef2]
    Former-commit-id: d687405e4fbe1b33b414b2ea9937534cf6ccd990

--- scripts/train.sh
+++ scripts/train.sh
@@ -3,5 +3,5 @@ accelerate launch --multi_gpu --num_processes 7 --mixed_precision bf16 opensora/
   --ae ucf101_stride4x4x4 \
   --data-path "/remote-home/yeyang/sky_timelapse/sky_train/" --extras 1 \
   --sample-rate 3 --num-frames 128 --max-image-size 256 \
-  --max-train-steps 1000000 --local-batch-size 5 --lr 1e-4 \
+  --max-train-steps 1000000 --local-batch-size 3 --lr 1e-4 \
   --ckpt-every 500 --log-every 50 --gradient-checkpointing --mixed-precision
