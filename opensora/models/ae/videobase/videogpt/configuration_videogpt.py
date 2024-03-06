cf1b771083baed6a326253d5ca55e10204889862
qqingzheng <2533221180@qq.com>
2024-03-06 15:08:36 +0000

    [fix] fix a bug when using ucf101_stride4x4x4

--- opensora/models/ae/videobase/videogpt/configuration_videogpt.py
+++ opensora/models/ae/videobase/videogpt/configuration_videogpt.py
@@ -11,7 +11,7 @@ class VideoGPTConfiguration:
     resolution: int
     sequence_length: int
     downsample: str
-    
+    no_pos_embd: bool
     def __post_init__(self):
         if isinstance(self.downsample, str):
             self.downsample = tuple(map(int, self.downsample.split(',')))
