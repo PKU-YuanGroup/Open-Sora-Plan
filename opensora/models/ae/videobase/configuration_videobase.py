97077ddddb9cad6a74979c498b3f4786e4694231
qqingzheng <2533221180@qq.com>
2024-03-06 13:45:16 +0000

    [refactor] reformat videogpt, support training videogpt on accelerate

---
+++ opensora/models/ae/videobase/configuration_videobase.py
@@ -0,0 +1,12 @@
+from dataclasses import dataclass
+
+
+"""
+We are currently working on the abstraction of the code structure, 
+so there is no content here for the moment. 
+It will be gradually optimized over time. 
+Contributions from the open-source community are welcome.
+"""
+@dataclass
+class VideoBaseConfiguration:
+    ...
---
+++ opensora/models/ae/videobase/modeling_videobase.py
@@ -0,0 +1,14 @@
+from torch import nn
+
+"""
+We are currently working on the abstraction of the code structure, 
+so there is no content here for the moment. 
+It will be gradually optimized over time. 
+Contributions from the open-source community are welcome.
+"""
+class VideoBaseAE(nn.Module):
+    def __init__(self, *args, **kwargs) -> None:
+        super().__init__(*args, **kwargs)
+        
+    def download_and_load_model(self):
+        return None
