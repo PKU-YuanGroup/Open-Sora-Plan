eaf7aff3abf9abf9305fb27ade78b4202189ab37
qqingzheng <2533221180@qq.com>
2024-03-06 08:16:58 +0000

    first commit
    Former-commit-id: d1dd19a281bd5ff9b2aca577bf5141e8c6550af1 [formerly 5684f6f6b824d1d8c14240676257fa5ad2a2104b]
    Former-commit-id: 1e61dacb7340e21bda56103710e1013cfd2425b4

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,7 +1,7 @@
 torchrun  --nproc_per_node=1 --master_port=29501 opensora/train/train.py \
-  --model DiT-XL/122 --dataset ucf101\
+  --model DiT-XL/122 --dataset ucf101 \
   --ae ucf101_stride4x4x4 \
-  --data-path /remote-home/yeyang/UCF-101 --num-classes 101 \
+  --data-path UCF-101 --num-classes 101 \
   --sample-rate 2 --num-frames 8 --max-image-size 128 --clip-grad-norm 1 \
   --epochs 14000 --global-batch-size 2 --lr 1e-4 \
-  --ckpt-every 1000 --log-every 50
+  --ckpt-every 1000 --log-every 50  
