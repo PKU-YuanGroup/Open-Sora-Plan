d0ff7cc6aad41081cbc853c21d32a26419552dc1
qqingzheng <2533221180@qq.com>
2024-03-07 09:20:12 +0000

    update train.sh

--- scripts/train.sh
+++ scripts/train.sh
@@ -1,7 +1,8 @@
-accelerate launch --multi_gpu --num_processes 7 --mixed_precision bf16 opensora/train/train.py \
-  --model Latte-XL/122 --dataset sky \
+accelerate launch --config_file scripts/accelerate_configs/default_config.yaml \
+  opensora/train/train.py \
+  --model Latte-XL/122 --dataset ucf101 \
   --ae ucf101_stride4x4x4 \
-  --data-path "/remote-home/yeyang/sky_timelapse/sky_train/" --extras 1 \
+  --data-path "/data03/dragon_proj/Open-Sora-Plan/UCF-101/train/" --extras 1 \
   --sample-rate 3 --num-frames 128 --max-image-size 256 \
   --max-train-steps 1000000 --local-batch-size 3 --lr 1e-4 \
   --ckpt-every 500 --log-every 50 --gradient-checkpointing --mixed-precision
