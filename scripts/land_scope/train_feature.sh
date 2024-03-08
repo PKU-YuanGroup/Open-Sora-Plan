71b08a27ddf3c5d4cdc84360df4314f9282b6a02
LinB203 <2267330597@qq.com>
2024-03-09 01:03:05 +0800

    extract script

--- scripts/land_scope/train_feature.sh
+++ scripts/land_scope/train_feature.sh
@@ -1,9 +1,9 @@
 
 
-accelerate launch --multi_gpu --num_processes 8  opensora/train/train_with_feature.py \
+accelerate launch --multi_gpu --num_processes 7  opensora/train/train_with_feature.py \
   --model Latte-XL/122 --dataset landscope_feature \
   --ae stabilityai/sd-vae-ft-mse \
-  --data-path "/remote-home/yeyang/train_movie/*/*.mp4" --extras 1 \
+  --data-path "/remote-home/yeyang/train_movie/*/*256.npy" --extras 1 \
   --sample-rate 3 --num-frames 128 --max-image-size 256 \
   --max-train-steps 1000000 --local-batch-size 5 --lr 1e-4 \
-  --ckpt-every 500 --log-every 50 --gradient-checkpointing
+  --ckpt-every 500 --log-every 50  --gradient-checkpointing
