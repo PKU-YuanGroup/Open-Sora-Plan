1e6d6ace7a0963149e228813801210b022aceacf
ZongyingLin <2218155980@QQ.COM>
2024-03-08 20:45:47 +0800

    [Updata] updata the video super resolution
    updata the run.py and add README.md

--- opensora/models/super_resolution/run.py
+++ opensora/models/super_resolution/run.py
@@ -7,6 +7,9 @@ import shutil
 from PIL import Image
 import re
 import imageio.v2 as imageio
+import threading
+from concurrent.futures import ThreadPoolExecutor
+import time
 
 def replace_filename(original_path, suffix):
 
@@ -41,9 +44,18 @@ def bicubic_upsample_opencv(input_image_path, output_image_path, scale_factor):
     new_height = int(original_height * scale_factor)
     
     upsampled_img = cv2.resize(img, (new_width, new_height), interpolation=cv2.INTER_CUBIC)
-    
     cv2.imwrite(output_image_path, upsampled_img)
 
+
+def process_frame(frame_count, frame, temp_LR_folder_path, temp_HR_folder_path, SR):
+    frame_path = os.path.join(temp_LR_folder_path, f"frame_{frame_count}{SR}.png")
+    cv2.imwrite(frame_path, frame)
+    HR_frame_path = os.path.join(temp_HR_folder_path, f"frame_{frame_count}.png")
+
+    if SR == 'x4': 
+        bicubic_upsample_opencv(frame_path, HR_frame_path, 4)
+    elif SR == 'x2': 
+        bicubic_upsample_opencv(frame_path, HR_frame_path, 2)
 
 def video_sr(args):
     file_name = os.path.basename(args.input_dir)
@@ -69,44 +81,58 @@ def video_sr(args):
         print("Error opening video file.")
         return
 
+    t1 = time.time()
     frame_count = 0
+    frames_to_process = []
     while cap.isOpened():
         ret, frame = cap.read()
         if not ret:
             break
-    
-        frame_path = os.path.join(temp_LR_folder_path, f"frame_{frame_count}{args.SR}.png")
-        cv2.imwrite(frame_path, frame)
-        HR_frame_path = os.path.join(temp_HR_folder_path, f"frame_{frame_count}.png")
-    
-        if args.SR == 'x4': 
-            bicubic_upsample_opencv(frame_path, HR_frame_path, 4)
-        if args.SR == 'x2': 
-            bicubic_upsample_opencv(frame_path, HR_frame_path, 2)
-        frame_count +=1 
+        frames_to_process.append((frame_count, frame))
+        frame_count += 1
 
+    with ThreadPoolExecutor(max_workers = args.mul_numwork) as executor:
+        for frame_count, frame in frames_to_process:
+            executor.submit(process_frame, frame_count, frame, temp_LR_folder_path, temp_HR_folder_path, args.SR)
+    
+    print("total frames:",frame_count)
+    print("fps :",cap.get(cv2.CAP_PROP_FPS))
+    
+    t2 = time.time()
+    print('mul threads: ',t2 - t1,'s')
     # progress all frames in video 
     image_sr(args)
 
+    t3 = time.time()
+    print('image super resolution: ',t3 - t2,'s')
     # recover video form all frames
     frame_files = sorted(os.listdir(result_temp), key=extract_number)
     video_frames = [imageio.imread(os.path.join(result_temp, frame_file)) for frame_file in frame_files]
-    # get video config
     fps = cap.get(cv2.CAP_PROP_FPS)
     imageio.mimwrite(video_output_path, video_frames, fps=fps, quality=9)  
     
+    t4 = time.time()
+    print('tranformer frames to  video: ',t4 - t3,'s')
     # release all resources
     cap.release()
-
     delete_temp_folder(os.path.dirname(temp_LR_folder_path))
     delete_temp_folder(temp_HR_folder_path)
     delete_temp_folder(os.path.join(args.root_path, f'results'))
+    
+    t5 = time.time()
+    print('delete time: ',t5 - t4,'s')
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser(description="RGT for Video Super-Resolution")
+    # make sure you SR is match with the ckpt_path
     parser.add_argument("--SR", type=str, choices=['x2', 'x4'], default='x4', help='image resolution')
+    parser.add_argument("--ckpt_path", type=str, default = "/remote-home/lzy/RGT/experiments/pretrained_models/RGT_x4.pth")
+    
+    parser.add_argument("--root_path", type=str, default = "/remote-home/lzy/RGT")
     parser.add_argument("--input_dir", type=str, default= "/remote-home/lzy/RGT/datasets/video/video_test1.mp4")
     parser.add_argument("--output_dir", type=str, default= "/remote-home/lzy/RGT/datasets/video_output")
-    parser.add_argument("--root_path", type=str, default = "/remote-home/lzy/RGT")
+    
+    parser.add_argument("--mul_numwork", type=int, default = 16, help ='max_workers to execute Multi')
+    parser.add_argument("--use_chop", type= bool, default = True,  help ='use_chop: True  # True to save memory, if img too large')
     args = parser.parse_args()
     video_sr(args)
