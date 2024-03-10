a7165b506e8a6716bb4a5b2012ec09c6cf0d9f30
LinB203 <2267330597@qq.com>
2024-03-10 18:34:37 +0800

    clean script
    Former-commit-id: 02c61f63a8539a31516b92f5bb7c2d2e428ca7d7

--- scripts/land_scope/train_feature.sh
+++
@@ -1,9 +0,0 @@
-
-
-accelerate launch --multi_gpu --num_processes 7  --mixed_precision bf16 opensora/train/train_with_feature.py \
-  --model Latte-XL/122 --dataset landscope_feature \
-  --ae stabilityai/sd-vae-ft-mse \
-  --data-path "/remote-home/yeyang/train_movie/*/*256.npy" --extras 1 \
-  --sample-rate 3 --num-frames 128 --max-image-size 256 \
-  --max-train-steps 1000000 --local-batch-size 5 --lr 1e-4 \
-  --ckpt-every 500 --log-every 50  --gradient-checkpointing
