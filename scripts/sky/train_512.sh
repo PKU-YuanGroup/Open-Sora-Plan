b57709108a0b91bc1e8e2f6d278ac6ffce86f7f9
LinB203 <2267330597@qq.com>
2024-03-10 18:08:23 +0800

    train 1080p video
    Former-commit-id: 74625142ef892f9e7fa459b2f98f2f7ae5b36d6a

--- scripts/sky/train_512.sh
+++ scripts/sky/train_512.sh
@@ -1,8 +1,10 @@
 
-accelerate launch --multi_gpu --num_processes 8 --mixed_precision bf16 opensora/train/train.py \
+accelerate launch \
+  --config_file scripts/accelerate_configs/ddp_config.yaml \
+  opensora/train/train.py \
   --model Latte-XL/122 --dataset sky \
   --ae stabilityai/sd-vae-ft-mse \
   --data-path "/remote-home/yeyang/sky_timelapse/sky_train/" --extras 1 \
   --sample-rate 3 --num-frames 16 --max-image-size 512 \
   --max-train-steps 1000000 --local-batch-size 2 --lr 1e-4 \
-  --ckpt-every 500 --log-every 50 --gradient-checkpointing --attention_mode flash
+  --ckpt-every 500 --log-every 50 --gradient-checkpointing --attention_mode flash  --mixed_precision bf16
