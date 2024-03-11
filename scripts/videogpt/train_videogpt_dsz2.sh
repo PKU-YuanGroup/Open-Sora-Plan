a360da1ec75d24f74b9527a72fba5a2eb6aebc10
sennnnn <cyanlaser@stu.pku.edu.cn>
2024-03-11 20:35:01 +0800

    update videogpt.
    Former-commit-id: 2e211563c684a8b6ef62dfc0c4f1897224dfb58d

--- scripts/videogpt/train_videogpt_dsz2.sh
+++ scripts/videogpt/train_videogpt_dsz2.sh
@@ -1,4 +1,4 @@
-export ACCELERATE_GRADIENT_ACCUMULATION_STEPS=8
+export ACCELERATE_GRADIENT_ACCUMULATION_STEPS=4
 
 accelerate launch \
   --config_file scripts/accelerate_configs/deepspeed_zero2_config.yaml \
@@ -11,18 +11,18 @@ accelerate launch \
   --learning_rate 1e-5 \
   --weight_decay 0. \
   --num_train_epochs 2 \
-  --embedding_dim 512 \
   --lr_scheduler_type cosine \
   --max_grad_norm 1.0 \
   --save_strategy steps \
   --save_total_limit 5 \
   --logging_steps 5 \
-  --save_steps 5 \
+  --save_steps 1000 \
   --n_codes 2048 \
-  --n_hiddens 240 \
+  --n_hiddens 512 \
+  --embedding_dim 4 \
   --n_res_layers 4 \
-  --downsample "4,4,4" \
-  --resolution 336 \
+  --downsample "4,8,8" \
+  --resolution 128 \
   --sequence_length 16 \
   --bf16 False \
   --fp16 True \
