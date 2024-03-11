1321e1f977a98c7db891c267c78d9996a2018e83
sennnnn <cyanlaser@stu.pku.edu.cn>
2024-03-11 11:51:46 +0800

    refactor dit for supporting deepspeed.
    Former-commit-id: e1909745cb05ea2f28f3a391c194f7656593a236

--- opensora/models/diffusion/__init__.py
+++ opensora/models/diffusion/__init__.py
@@ -1,5 +1,7 @@
-from .dit.dit import DiT_models
+from .dit.modeling_dit import DiT_models
 from .latte.modeling_latte import Latte_models
+# NOTE: vanilla model definition, which only supports DDP training.
+# from .dit.dit import DiT_models
 # from .latte.latte import Latte_models
 from .latte_t2v.latte_t2v import LatteT2V
 
