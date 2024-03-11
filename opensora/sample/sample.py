0e2a86b2ac33c5cb12868563470baf7f9180effa
LinB203 <2267330597@qq.com>
2024-03-11 15:44:53 +0800

    clean dit
    Former-commit-id: 117ec4196a6f2cd83da223b993b14e28f7a693f8

--- opensora/sample/sample.py
+++ opensora/sample/sample.py
@@ -10,6 +10,7 @@ import os
 import sys
 
 from accelerate import Accelerator
+from tqdm import tqdm
 
 from opensora.dataset import ae_denorm
 from opensora.models.ae import ae_channel_config, getae, ae_stride_config
@@ -23,8 +24,8 @@ import argparse
 from einops import rearrange
 import imageio
 
-torch.backends.cuda.matmul.allow_tf32 = True
-torch.backends.cudnn.allow_tf32 = True
+# torch.backends.cuda.matmul.allow_tf32 = True
+# torch.backends.cudnn.allow_tf32 = True
 
 def main(args):
     # Setup PyTorch:
@@ -39,7 +40,7 @@ def main(args):
     using_cfg = args.cfg_scale > 1.0
 
     # Load model:
-    latent_size = args.image_size // ae_stride_config[args.ae][1]
+    latent_size = (args.image_size // ae_stride_config[args.ae][1], args.image_size // ae_stride_config[args.ae][2])
     args.latent_size = latent_size
     model = Diffusion_models[args.model](
         input_size=latent_size,
@@ -65,56 +66,45 @@ def main(args):
     diffusion = create_diffusion(str(args.num_sampling_steps))
     ae = getae(args).to(device)
 
-    # if args.attention_mode == 'flash':
-    #     ae.to(dtype=torch.bfloat16)
-    #     model.to(dtype=torch.bfloat16)
+    bar = tqdm(range(args.num_sample))
+    for i in bar:
+        # Create sampling noise:
+        z = torch.randn(1, args.num_frames // ae_stride_config[args.ae][0], model.in_channels, latent_size[0], latent_size[1], device=device)
 
-    # Labels to condition the model with (feel free to change):
+        # Setup classifier-free guidance:
+        if using_cfg:
+            z = torch.cat([z, z], 0)
+            y = torch.randint(0, args.num_classes, (1,), device=device)
+            y_null = torch.tensor([args.num_classes] * 1, device=device)
+            y = torch.cat([y, y_null], dim=0)
+            model_kwargs = dict(y=y, cfg_scale=args.cfg_scale)
+            sample_fn = model.forward_with_cfg
+        else:
+            sample_fn = model.forward
+            model_kwargs = dict(y=None)
 
-    # Create sampling noise:
-    # if args.attention_mode == 'flash':
-    #     z = torch.randn(1, args.num_frames // ae_stride_config[args.ae][0], model.in_channels, latent_size, latent_size, dtype=torch.bfloat16, device=device)
-    # else:
-    z = torch.randn(1, args.num_frames // ae_stride_config[args.ae][0], model.module.in_channels, latent_size, latent_size, device=device)
+        # Sample images:
+        if args.sample_method == 'ddim':
+            samples = diffusion.ddim_sample_loop(
+                sample_fn, z.shape, z, clip_denoised=False, model_kwargs=model_kwargs, progress=True, device=device
+            )
+        elif args.sample_method == 'ddpm':
+            samples = diffusion.p_sample_loop(
+                sample_fn, z.shape, z, clip_denoised=False, model_kwargs=model_kwargs, progress=True, device=device
+            )
 
-    # Setup classifier-free guidance:
-    if using_cfg:
-        z = torch.cat([z, z], 0)
-        y = torch.randint(0, args.num_classes, (1,), device=device)
-        y_null = torch.tensor([args.num_classes] * 1, device=device)
-        y = torch.cat([y, y_null], dim=0)
-        model_kwargs = dict(y=y, cfg_scale=args.cfg_scale)
-        sample_fn = model.forward_with_cfg
-    else:
-        sample_fn = model.forward
-        model_kwargs = dict(y=None)
+        with torch.no_grad():
+            samples = ae.decode(samples)
+        # Save and display images:
 
-    # Sample images:
-    if args.sample_method == 'ddim':
-        samples = diffusion.ddim_sample_loop(
-            sample_fn, z.shape, z, clip_denoised=False, model_kwargs=model_kwargs, progress=True, device=device
-        )
-    elif args.sample_method == 'ddpm':
-        samples = diffusion.p_sample_loop(
-            sample_fn, z.shape, z, clip_denoised=False, model_kwargs=model_kwargs, progress=True, device=device
-        )
+        if not os.path.exists(args.save_video_path):
+            os.makedirs(args.save_video_path)
 
-    # if args.attention_mode == 'flash':
-    #     samples = samples.to(dtype=torch.bfloat16)
-
-    with torch.no_grad():
-        samples = ae.decode(samples)
-    # Save and display images:
-
-    if not os.path.exists(args.save_video_path):
-        os.makedirs(args.save_video_path)
-
-
-    video_ = (ae_denorm[args.ae](samples[0]) * 255).add_(0.5).clamp_(0, 255).to(dtype=torch.uint8).cpu().permute(0, 2, 3, 1).contiguous()
-    video_save_path = os.path.join(args.save_video_path, 'sample' + '.mp4')
-    print(video_save_path)
-    imageio.mimwrite(video_save_path, video_, fps=args.fps, quality=9)
-    print('save path {}'.format(args.save_video_path))
+        video_ = (ae_denorm[args.ae](samples[0]) * 255).add_(0.5).clamp_(0, 255).to(dtype=torch.uint8).cpu().permute(0, 2, 3, 1).contiguous()
+        video_save_path = os.path.join(args.save_video_path, f'sample_{i:03d}' + '.mp4')
+        print(video_save_path)
+        imageio.mimwrite(video_save_path, video_, fps=args.fps, quality=9)
+        print('save path {}'.format(args.save_video_path))
 
 
 if __name__ == "__main__":
@@ -126,12 +116,13 @@ if __name__ == "__main__":
     parser.add_argument("--fps", type=int, default=10)
     parser.add_argument("--num-classes", type=int, default=101)
     parser.add_argument("--num-frames", type=int, default=16)
-    parser.add_argument("--image-size", type=int, default=256, choices=[256, 512])
+    parser.add_argument("--image-size", type=int, default=256)
     parser.add_argument("--extras", type=int, default=1)
     parser.add_argument("--num-sampling-steps", type=int, default=250)
+    parser.add_argument("--num-sample", type=int, default=1)
     parser.add_argument("--cfg-scale", type=float, default=1.0)
     parser.add_argument("--use-compile", action="store_true")
     parser.add_argument("--sample-method", type=str, default='ddpm')
-    parser.add_argument("--attention_mode", type=str, choices=['xformers', 'math', 'flash'], default="math")
+    parser.add_argument("--attention-mode", type=str, choices=['xformers', 'math', 'flash'], default="math")
     args = parser.parse_args()
     main(args)
