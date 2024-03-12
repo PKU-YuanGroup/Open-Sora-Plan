c9660af891c0dae1f51a1075ef5b2df012f65e06
sennnnn <cyanlaser@stu.pku.edu.cn>
2024-03-12 10:30:41 +0800

    Update training arguments of videogpt training.

--- scripts/videogpt/train_videogpt_dsz2.sh
+++ scripts/videogpt/train_videogpt_dsz2.sh
@@ -1,14 +1,14 @@
-export ACCELERATE_GRADIENT_ACCUMULATION_STEPS=4
+export ACCELERATE_GRADIENT_ACCUMULATION_STEPS=1
 
 accelerate launch \
   --config_file scripts/accelerate_configs/deepspeed_zero2_config.yaml \
   opensora/train/train_videogpt.py \
   --do_train \
   --seed 1234 \
-  --data_path datasets/UCF-101 \
-  --per_device_train_batch_size 8 \
+  --data_path "datasets/UCF-101/" \
+  --per_device_train_batch_size 32  \
   --gradient_accumulation_steps $ACCELERATE_GRADIENT_ACCUMULATION_STEPS \
-  --learning_rate 1e-5 \
+  --learning_rate 7e-4 \
   --weight_decay 0. \
   --num_train_epochs 2 \
   --lr_scheduler_type cosine \
@@ -16,15 +16,15 @@ accelerate launch \
   --save_strategy steps \
   --save_total_limit 5 \
   --logging_steps 5 \
-  --save_steps 1000 \
-  --n_codes 2048 \
-  --n_hiddens 512 \
+  --save_steps 10000 \
+  --n_codes 1024 \
+  --n_hiddens 240 \
   --embedding_dim 4 \
   --n_res_layers 4 \
-  --downsample "4,8,8" \
+  --downsample "4,4,4" \
   --resolution 128 \
   --sequence_length 16 \
-  --bf16 False \
-  --fp16 True \
-  --output_dir results/videogpt \
+  --output_dir results/videogpt_444_128 \
+  --bf16 True \
+  --fp16 False \
   --report_to tensorboard
