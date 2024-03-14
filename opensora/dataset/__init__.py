d89d7c5b274d32e96d903b45a800e5453a635cba
LinB203 <2267330597@qq.com>
2024-03-14 08:51:10 +0800

    del t2v

--- opensora/dataset/__init__.py
+++ opensora/dataset/__init__.py
@@ -9,9 +9,9 @@ from torchvision.transforms import Lambda
 from .transform import ToTensorVideo, TemporalRandomCrop, RandomHorizontalFlipVideo, CenterCropResizeVideo
 from .ucf101 import UCF101
 from .sky_datasets import Sky
-from .t2v_dataset import T2V_dataset
 
 ae_norm = {
+    "checkpoint-14000": Lambda(lambda x: x - 0.5),
     "bair_stride4x2x2": Lambda(lambda x: x - 0.5),
     "ucf101_stride4x4x4": Lambda(lambda x: x - 0.5),
     "kinetics_stride4x4x4": Lambda(lambda x: x - 0.5),
@@ -24,6 +24,7 @@ ae_norm = {
 
 }
 ae_denorm = {
+    "checkpoint-14000": lambda x: x + 0.5,
     "bair_stride4x2x2": lambda x: x + 0.5,
     "ucf101_stride4x4x4": lambda x: x + 0.5,
     "kinetics_stride4x4x4": lambda x: x + 0.5,
@@ -38,13 +39,7 @@ ae_denorm = {
 def getdataset(args):
     temporal_sample = TemporalRandomCrop(args.num_frames * args.sample_rate)  # 16 x
     norm_fun = ae_norm[args.ae]
-    if args.dataset == 'landscope_feature':
-        return LandscopeFeature(args, temporal_sample=temporal_sample)
-    elif args.dataset == 'landscope_video_feature':
-        return LandscopeVideoFeature(args)
-    elif args.dataset == 'sky_feature':
-        return SkyFeature(args, temporal_sample=temporal_sample)
-    elif args.dataset == 'ucf101':
+    if args.dataset == 'ucf101':
         transform = Compose(
             [
                 ToTensorVideo(),  # TCHW
@@ -58,11 +53,9 @@ def getdataset(args):
         transform_sky = transforms.Compose([
             ToTensorVideo(),
             CenterCropResizeVideo(args.max_image_size),
-            # video_transforms.RandomHorizontalFlipVideo(),
+            RandomHorizontalFlipVideo(p=0.5),
             norm_fun
         ])
         return Sky(args, transform=transform_sky, temporal_sample=temporal_sample)
-    elif args.dataset == 't2v':
-        return T2V_dataset(csv_path=args.csv_path, video_folder=args.video_folder, sample_size=args.image_size, sample_stride=args.sample_rate)
     else:
         raise NotImplementedError(args.dataset)
--- opensora/models/ae/videobase/vqvae/__init__.py
+++ opensora/models/ae/videobase/vqvae/__init__.py
@@ -1,4 +1,35 @@
+from einops import rearrange
+from torch import nn
+
 from .configuration_vqvae import VQVAEConfiguration
 from .modeling_vqvae import VQVAEModel
 from .trainer_vqvae import VQVAETrainer
 from .dataset_vqvae import VQVAEDataset
+
+
+videovqvae = [
+    "bair_stride4x2x2",
+    "ucf101_stride4x4x4",
+    "kinetics_stride4x4x4",
+    "kinetics_stride2x4x4",
+]
+videovae = []
+
+class VideoGPTVQVAEWrapper(nn.Module):
+    def __init__(self, ckpt='kinetics_stride4x4x4'):
+        super(VideoGPTVQVAEWrapper, self).__init__()
+        if ckpt in videovqvae:
+            self.vqvae = VQVAEModel.download_and_load_model(ckpt)
+        else:
+            self.vqvae = VQVAEModel.load_from_checkpoint(ckpt)
+    def encode(self, x):  # b c t h w
+        x = self.vqvae.pre_vq_conv(self.vqvae.encoder(x))
+        x = rearrange(x, 'b c t h w -> b t c h w').contiguous()
+        x = x * 0.18215
+        return x
+    def decode(self, x):
+        x = rearrange(x / 0.18215, 'b t c h w -> b c t h w').contiguous()
+        vq_output = self.vqvae.codebook(x)
+        x = self.vqvae.decoder(self.vqvae.post_vq_conv(vq_output['embeddings']))
+        x = rearrange(x, 'b c t h w -> b t c h w').contiguous()
+        return x
