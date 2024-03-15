8b2a794095f2deb9b1e8090486cb8242bf81031d
LinB203 <2267330597@qq.com>
2024-03-15 19:09:18 +0800

    update scripts

--- scripts/sky/train_256.sh
+++ scripts/sky/train_256.sh
@@ -19,25 +19,25 @@ accelerate launch \
     --attention-mode flash \
     --mixed-precision bf16
 
-#for debug
-#accelerate launch \
-#    --num_processes 1 \
-#    opensora/train/train.py \
-#    --model Latte-XL/122 \
-#    --dataset sky \
-#    --ae checkpoint-14000 \
-#    --data-path /remote-home/yeyang/sky_timelapse/sky_train/ \
-#    --extras 1 \
-#    --sample-rate 2 \
-#    --num-frames 16 \
-#    --max-image-size 256 \
-#    --max-train-steps 1000000 \
-#    --local-batch-size 2 \
-#    --lr 1e-4 \
-#    --ckpt-every 500 \
-#    --log-every 50 \
-#    --gradient-checkpointing \
-#    --attention-mode flash \
-#    --mixed-precision bf16 \
-#    --num-workers 0 \
-#    --pretrained PixArt-XL-2-256x256.pth
+for debug
+HF_DATASETS_OFFLINE=1 TRANSFORMERS_OFFLINE=1 accelerate launch \
+    --num_processes 1 \
+    opensora/train/train.py \
+    --model Latte-XL/122 \
+    --dataset sky \
+    --ae checkpoint-14000 \
+    --data-path /remote-home/yeyang/sky_timelapse/sky_train/ \
+    --extras 1 \
+    --sample-rate 2 \
+    --num-frames 8 \
+    --max-image-size 128 \
+    --max-train-steps 1000000 \
+    --local-batch-size 2 \
+    --lr 1e-4 \
+    --ckpt-every 500 \
+    --log-every 50 \
+    --gradient-checkpointing \
+    --attention-mode flash \
+    --mixed-precision bf16 \
+    --num-workers 0 \
+    --pretrained PixArt-XL-2-256x256.pth
