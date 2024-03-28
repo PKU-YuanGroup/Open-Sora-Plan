24298df5d374d5aaa5696f6200d4e23ae887715f
LinB203 <2267330597@qq.com>
2024-03-28 19:57:15 +0800

    update vae

--- opensora/models/ae/videobase/causal_vae/__init__.py
+++ opensora/models/ae/videobase/causal_vae/__init__.py
@@ -2,3 +2,20 @@ from .configuration_causalvae import CausalVAEConfiguration
 from .dataset_causalvae import CausalVAEDataset
 from .modeling_causalvae import CausalVAEModel
 from .trainer_causalvae import CausalVAETrainer
+
+from einops import rearrange
+from torch import nn
+
+class CausalVAEModelWrapper(nn.Module):
+    def __init__(self, ckpt):
+        super(CausalVAEModelWrapper, self).__init__()
+        self.vae = CausalVAEModel.load_from_checkpoint(ckpt)
+    def encode(self, x):  # b c t h w
+        # x = self.vae.encode(x).sample()
+        x = self.vae.encode(x).sample().mul_(0.18215)
+        return x
+    def decode(self, x):
+        # x = self.vae.decode(x)
+        x = self.vae.decode(x / 0.18215)
+        x = rearrange(x, 'b c t h w -> b t c h w').contiguous()
+        return x
