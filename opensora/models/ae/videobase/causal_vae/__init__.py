ceb45b8b4103f2a375e29273749c02a73017fc46
LinB203 <2267330597@qq.com>
2024-04-06 19:26:08 +0800

    refactor vae to hf
    Former-commit-id: 0ee29000d6554b181fb10d0a9111b44616f8f4eb [formerly 8867473fb32644d75295bcc11ec3469f9c0fd3e9]
    Former-commit-id: 1493681f6f54e308a55d1b46e2326713648f7e4d

--- opensora/models/ae/videobase/causal_vae/__init__.py
+++ opensora/models/ae/videobase/causal_vae/__init__.py
@@ -1,3 +1,5 @@
+import os.path
+
 import torch
 
 from .configuration_causalvae import CausalVAEConfiguration
@@ -9,9 +11,11 @@ from einops import rearrange
 from torch import nn
 
 class CausalVAEModelWrapper(nn.Module):
-    def __init__(self, ckpt):
+    def __init__(self, model_path, subfolder=None, cache_dir=None):
         super(CausalVAEModelWrapper, self).__init__()
-        self.vae = CausalVAEModel.load_from_checkpoint(ckpt)
+        # if os.path.exists(ckpt):
+        # self.vae = CausalVAEModel.load_from_checkpoint(ckpt)
+        self.vae = CausalVAEModel.from_pretrained(model_path, subfolder=subfolder, cache_dir=cache_dir)
     def encode(self, x):  # b c t h w
         # x = self.vae.encode(x).sample()
         x = self.vae.encode(x).sample().mul_(0.18215)
