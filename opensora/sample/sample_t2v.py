379d89e0eb406f47f95c2bbb3e6f9895135336e9
LinB203 <2267330597@qq.com>
2024-04-06 13:31:37 +0800

    hw

--- opensora/sample/sample_t2v.py
+++ opensora/sample/sample_t2v.py
@@ -1,131 +1,171 @@
-# All rights reserved.
-
-# This source code is licensed under the license found in the
-# LICENSE file in the root directory of this source tree.
-
-"""
-Sample new images from a pre-trained Latte.
-"""
 import os
-import sys
-
-from accelerate import Accelerator
-from tqdm import tqdm
-from transformers import AutoTokenizer
-
-from opensora.dataset import ae_denorm
-from opensora.models.ae import ae_channel_config, getae, ae_stride_config, getae_wrapper
-from opensora.models.ae.videobase import CausalVQVAEModelWrapper, CausalVAEModelWrapper
-from opensora.models.diffusion import Diffusion_models
-from opensora.models.diffusion.diffusion import create_diffusion_T as create_diffusion
-from opensora.models.diffusion.latte.modeling_latte import LatteT2V
-from opensora.models.text_encoder import get_text_enc
-from opensora.utils.utils import find_model
-
 import torch
 import argparse
+import torchvision
 
-from einops import rearrange
+from diffusers.schedulers import (DDIMScheduler, DDPMScheduler, PNDMScheduler,
+                                  EulerDiscreteScheduler, DPMSolverMultistepScheduler,
+                                  HeunDiscreteScheduler, EulerAncestralDiscreteScheduler,
+                                  DEISMultistepScheduler, KDPM2AncestralDiscreteScheduler)
+from diffusers.schedulers.scheduling_dpmsolver_singlestep import DPMSolverSinglestepScheduler
+from diffusers.models import AutoencoderKL, AutoencoderKLTemporalDecoder
+from omegaconf import OmegaConf
+from torchvision.utils import save_image
+from transformers import T5EncoderModel, T5Tokenizer, AutoTokenizer
+
+import os, sys
+
+from opensora.models.ae import ae_stride_config, getae, getae_wrapper
+from opensora.models.ae.videobase import CausalVQVAEModelWrapper, CausalVAEModelWrapper
+from opensora.models.diffusion.latte.modeling_latte import LatteT2V
+from opensora.models.text_encoder import get_text_enc
+from opensora.utils.utils import save_video_grid
+
+sys.path.append(os.path.split(sys.path[0])[0])
+from pipeline_videogen import VideoGenPipeline
+
 import imageio
-
-torch.backends.cuda.matmul.allow_tf32 = True
-torch.backends.cudnn.allow_tf32 = True
 
 
 def main(args):
-    # Setup PyTorch:
     # torch.manual_seed(args.seed)
     torch.set_grad_enabled(False)
-    assert torch.cuda.is_available(), "Training currently requires at least one GPU."
+    device = "cuda" if torch.cuda.is_available() else "cpu"
 
-    # Setup accelerator:
-    accelerator = Accelerator(mixed_precision=args.mixed_precision)
-    device = accelerator.device
-    weight_dtype = torch.float32
-    if accelerator.mixed_precision == "fp16":
-        weight_dtype = torch.float16
-    elif accelerator.mixed_precision == "bf16":
-        weight_dtype = torch.bfloat16
-
-    # Load model:
-    latent_size = (args.image_size // ae_stride_config[args.ae][1], args.image_size // ae_stride_config[args.ae][2])
-    args.latent_size = latent_size
-    model = LatteT2V.from_pretrained(args.ckpt, subfolder="model").to(device)
-    tokenizer = AutoTokenizer.from_pretrained(args.text_encoder_name, cache_dir='./cache_dir')
-    text_enc = get_text_enc(args).to(device).eval()
-    ae = getae(args).to(device).eval()
+    vae = getae(args).to(device, dtype=torch.float16)
+    if args.enable_tiling:
+        vae.vae.enable_tiling()
+        vae.vae.tile_overlap_factor = args.tile_overlap_factor
     if getae_wrapper(args) == CausalVQVAEModelWrapper or getae_wrapper(args) == CausalVAEModelWrapper:
         video_length = args.num_frames // ae_stride_config[args.ae][0] + 1
     else:
         video_length = args.num_frames // ae_stride_config[args.ae][0]
-    model.eval()  # important!
+    if args.force_images:
+        video_length = 1
+        ext = '.jpg'
+    else:
+        ext = 'mp4'
+    # Load model:
+    latent_size = (args.image_size // ae_stride_config[args.ae][1], args.image_size // ae_stride_config[args.ae][2])
+    args.latent_size = latent_size
+    vae.latent_size = latent_size
+    transformer_model = LatteT2V.from_pretrained(args.ckpt, subfolder="model", torch_dtype=torch.float16).to(device)
+    transformer_model.force_images = args.force_images
+    # tokenizer = AutoTokenizer.from_pretrained(args.text_encoder_name, cache_dir='./cache_dir')
+    # text_encoder = get_text_enc(args).to(device).eval()
+    tokenizer = T5Tokenizer.from_pretrained(args.text_encoder_name, cache_dir="cache_dir")
+    text_encoder = T5EncoderModel.from_pretrained(args.text_encoder_name, cache_dir="cache_dir", torch_dtype=torch.float16).to(device)
 
-    model = accelerator.prepare(model)
+    transformer_model.eval()  # important!
 
-    diffusion = create_diffusion(str(args.num_sampling_steps))
+    # set eval mode
+    transformer_model.eval()
+    vae.eval()
+    text_encoder.eval()
 
-    bar = tqdm(range(args.num_sample))
-    for i in bar:
-        # Create sampling noise:
-        z = torch.randn(1, model.module.in_channels, video_length, latent_size[0], latent_size[1], device=device)
+    if args.sample_method == 'DDIM':  #########
+        scheduler = DDIMScheduler()
+    elif args.sample_method == 'EulerDiscrete':
+        scheduler = EulerDiscreteScheduler()
+    elif args.sample_method == 'DDPM':  #############
+        scheduler = DDPMScheduler()
+    elif args.sample_method == 'DPMSolverMultistep':
+        scheduler = DPMSolverMultistepScheduler()
+    elif args.sample_method == 'DPMSolverSinglestep':
+        scheduler = DPMSolverSinglestepScheduler()
+    elif args.sample_method == 'PNDM':
+        scheduler = PNDMScheduler()
+    elif args.sample_method == 'HeunDiscrete':  ########
+        scheduler = HeunDiscreteScheduler()
+    elif args.sample_method == 'EulerAncestralDiscrete':
+        scheduler = EulerAncestralDiscreteScheduler()
+    elif args.sample_method == 'DEISMultistep':
+        scheduler = DEISMultistepScheduler()
+    elif args.sample_method == 'KDPM2AncestralDiscrete':  #########
+        scheduler = KDPM2AncestralDiscreteScheduler()
+    print('videogen_pipeline', device)
+    videogen_pipeline = VideoGenPipeline(vae=vae,
+                                         text_encoder=text_encoder,
+                                         tokenizer=tokenizer,
+                                         scheduler=scheduler,
+                                         transformer=transformer_model).to(device=device)
+    # videogen_pipeline.enable_xformers_memory_efficient_attention()
 
-        text_tokens_and_mask = tokenizer(
-            args.prompt,
-            max_length=args.model_max_length,
-            padding='max_length',
-            truncation=True,
-            return_attention_mask=True,
-            add_special_tokens=True,
-            return_tensors='pt'
-        )
-        input_ids = text_tokens_and_mask['input_ids'].to(device)
-        cond_mask = text_tokens_and_mask['attention_mask'].to(device)
-        cond = text_enc(input_ids, cond_mask)  # B L D
-        attn_mask = None
-        model_kwargs = dict(encoder_hidden_states=cond, attention_mask=attn_mask, encoder_attention_mask=cond_mask)
-        sample_fn = model.forward
+    if not os.path.exists(args.save_img_path):
+        os.makedirs(args.save_img_path)
 
-        # Sample images:
-        if args.sample_method == 'ddim':
-            samples = diffusion.ddim_sample_loop(
-                sample_fn, z.shape, z, clip_denoised=False, model_kwargs=model_kwargs, progress=True, device=device
-            )
-        elif args.sample_method == 'ddpm':
-            samples = diffusion.p_sample_loop(
-                sample_fn, z.shape, z, clip_denoised=False, model_kwargs=model_kwargs, progress=True, device=device
-            )
+    video_grids = []
+    if not isinstance(args.text_prompt, list):
+        args.text_prompt = [args.text_prompt]
+    for prompt in args.text_prompt:
+        print('Processing the ({}) prompt'.format(prompt))
+        videos = videogen_pipeline(prompt,
+                                   video_length=video_length,
+                                   height=args.image_size,
+                                   width=args.image_size,
+                                   num_inference_steps=args.num_sampling_steps,
+                                   guidance_scale=args.guidance_scale,
+                                   enable_temporal_attentions=not args.force_images,
+                                   num_images_per_prompt=1,
+                                   mask_feature=True,
+                                   ).video
+        try:
+            if args.force_images:
+                videos = videos[:, 0].permute(0, 3, 1, 2)  # b t h w c -> b c h w
+                save_image(videos / 255.0, os.path.join(args.save_img_path,
+                                                     prompt.replace(' ', '_') + f'{args.sample_method}_gs{args.guidance_scale}_s{args.num_sampling_steps}.{ext}'),
+                           nrow=1, normalize=True, value_range=(0, 1))  # t c h w
 
-        with torch.no_grad():
-            samples = ae.decode(samples)
-        # Save and display images:
+            else:
+                imageio.mimwrite(
+                    os.path.join(
+                        args.save_img_path,
+                        prompt.replace(' ', '_') + f'{args.sample_method}_gs{args.guidance_scale}_s{args.num_sampling_steps}.{ext}'
+                    ), videos[0],
+                    fps=args.fps, quality=9)  # highest quality is 10, lowest is 0
+        except:
+            print('Error when saving {}'.format(prompt))
+        video_grids.append(videos)
+    video_grids = torch.cat(video_grids, dim=0)
 
-        if not os.path.exists(args.save_video_path):
-            os.makedirs(args.save_video_path)
 
-        video_ = (ae_denorm[args.ae](samples[0]) * 255).add_(0.5).clamp_(0, 255).to(dtype=torch.uint8).cpu().permute(0, 2, 3, 1).contiguous()
-        video_save_path = os.path.join(args.save_video_path, f'sample_{i:03d}' + '.mp4')
-        print(video_save_path)
-        imageio.mimwrite(video_save_path, video_, fps=args.fps, quality=9)
-        print('save path {}'.format(args.save_video_path))
+    # torchvision.io.write_video(args.save_img_path + '_%04d' % args.run_time + '-.mp4', video_grids, fps=6)
+    if args.force_images:
+        save_image(video_grids / 255.0, os.path.join(args.save_img_path, f'{args.sample_method}_gs{args.guidance_scale}_s{args.num_sampling_steps}.{ext}'),
+                   nrow=4, normalize=True, value_range=(0, 1))
+    else:
+        video_grids = save_video_grid(video_grids)
+        imageio.mimwrite(os.path.join(args.save_img_path, f'{args.sample_method}_gs{args.guidance_scale}_s{args.num_sampling_steps}.{ext}'), video_grids, fps=args.fps, quality=9)
+
+    print('save path {}'.format(args.save_img_path))
+
+    # save_videos_grid(video, f"./{prompt}.gif")
 
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("--ckpt", type=str, default="")
     parser.add_argument("--model", type=str, default='Latte-XL/122')
-    parser.add_argument("--prompt", type=str, default='A woman')
-    parser.add_argument("--ae", type=str, default='stabilityai/sd-vae-ft-mse')
-    parser.add_argument("--save_video_path", type=str, default="./sample_videos/")
-    parser.add_argument("--fps", type=int, default=10)
-    parser.add_argument("--num_frames", type=int, default=16)
-    parser.add_argument("--image_size", type=int, default=256)
-    parser.add_argument("--num_sampling_steps", type=int, default=250)
-    parser.add_argument("--num_sample", type=int, default=1)
-    parser.add_argument("--cfg_scale", type=float, default=1.0)
-    parser.add_argument("--sample_method", type=str, default='ddpm')
-    parser.add_argument("--mixed_precision", type=str, default=None, choices=[None, "fp16", "bf16"])
-    parser.add_argument("--attention_mode", type=str, choices=['xformers', 'math', 'flash'], default="math")
+    parser.add_argument("--ae", type=str, default='CausalVAEModel_4x8x8')
     parser.add_argument("--text_encoder_name", type=str, default='DeepFloyd/t5-v1_1-xxl')
-    parser.add_argument("--model_max_length", type=int, default=120)
+    parser.add_argument("--save_img_path", type=str, default="./sample_videos/t2v")
+    parser.add_argument("--beta_start", type=float, default=0.0001)
+    parser.add_argument("--image_size", type=int, default=512)
+    parser.add_argument("--beta_end", type=float, default=0.02)
+    parser.add_argument("--guidance_scale", type=float, default=7.5)
+    parser.add_argument("--beta_schedule", type=str, default="linear")
+    parser.add_argument("--variance_type", type=str, default="learned_range")
+    parser.add_argument("--sample_method", type=str, default="PNDM")
+    parser.add_argument("--num_sampling_steps", type=int, default=50)
+    parser.add_argument("--fps", type=int, default=24)
+    parser.add_argument("--num_frames", type=int, default=65)
+    parser.add_argument("--run_time", type=int, default=0)
+    parser.add_argument("--text_prompt", nargs='+')
+
+    parser.add_argument('--force_images', action='store_true')
+
+    parser.add_argument('--tile_overlap_factor', type=float, default=0.25)
+    parser.add_argument('--enable_tiling', action='store_true')
     args = parser.parse_args()
+
     main(args)
