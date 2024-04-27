04bf0e8086ffd250ea5ffdcef037eb357664c5d0
BillionsRichard <sam.cheung@huawei.com>
2024-04-27 16:10:44 +0800

    CausalVAE的Encoder实现和文档网络结构不一致且内存低效

--- opensora/models/ae/videobase/causal_vae/modeling_causalvae.py
+++ opensora/models/ae/videobase/causal_vae/modeling_causalvae.py
@@ -112,18 +112,17 @@ class Encoder(nn.Module):
         )
 
     def forward(self, x):
-        hs = [self.conv_in(x)]
+        h = self.conv_in(x)
         for i_level in range(self.num_resolutions):
             for i_block in range(self.num_res_blocks):
-                h = self.down[i_level].block[i_block](hs[-1])
+                h = self.down[i_level].block[i_block](h)
                 if len(self.down[i_level].attn) > 0:
                     h = self.down[i_level].attn[i_block](h)
-                hs.append(h)
+
             if hasattr(self.down[i_level], "downsample"):
-                hs.append(self.down[i_level].downsample(hs[-1]))
+                h = self.down[i_level].downsample(h)
             if hasattr(self.down[i_level], "time_downsample"):
-                hs_down = self.down[i_level].time_downsample(hs[-1])
-                hs.append(hs_down)
+                h = self.down[i_level].time_downsample(h)                
 
         h = self.mid.block_1(h)
         h = self.mid.attn_1(h)
