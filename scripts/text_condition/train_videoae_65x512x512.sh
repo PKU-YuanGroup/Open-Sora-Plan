3e6ec8f5d44e759f472aa943bf48143b08c1c8b1
Eric Sheen <eric.sprog@gmail.com>
2024-05-18 12:19:18 -0700

    checkpoint

--- scripts/text_condition/train_videoae_65x512x512.sh
+++ scripts/text_condition/train_videoae_65x512x512.sh
@@ -8,16 +8,16 @@ accelerate launch \
     --text_encoder_name DeepFloyd/t5-v1_1-xxl \
     --dataset t2v \
     --ae CausalVAEModel_4x8x8 \
-    --ae_path "./Open-Sora-Plan-v1.0.0/vae" \
-    --data_path "./videos/captions.json" \
-    --video_folder "./videos" \
+    --ae_path CausalVAEModel_4x8x8 \
+    --data_path /remote-home1/dataset/sharegpt4v_path_cap_.json \
+    --video_folder /remote-home1/dataset/data_split_tt \
     --sample_rate 1 \
     --num_frames 65 \
     --max_image_size 512 \
     --gradient_checkpointing \
     --attention_mode xformers \
     --train_batch_size=2 \
-    --dataloader_num_workers 1 \
+    --dataloader_num_workers 10 \
     --gradient_accumulation_steps=1 \
     --max_train_steps=1000000 \
     --learning_rate=2e-05 \
@@ -28,7 +28,7 @@ accelerate launch \
     --checkpointing_steps=500 \
     --output_dir="t2v-f65-512-img16-videovae488-bf16-ckpt-xformers-bs4-lr2e-5-t5" \
     --allow_tf32 \
-    --pretrained "./Open-Sora-Plan-v1.0.0/65x512x512/diffusion_pytorch_model.safetensors" \
+    --pretrained "./Open-Sora-Plan-v1.0.0/65x256x256/diffusion_pytorch_model.safetensors" \
     --use_deepspeed \
     --model_max_length 300 \
     --use_image_num 16 \
