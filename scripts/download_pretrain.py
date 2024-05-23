a0d24377faa7392d92b1eb77b838cf2afc783401
Eric Sheen <eric.sprog@gmail.com>
2024-05-23 16:26:50 -0700

    download script

--- scripts/download_pretrain.py
+++ scripts/download_pretrain.py
@@ -1,22 +1,10 @@
 from huggingface_hub import hf_hub_download
-import os
 
-# Download t2v.pt
-t2v_path = hf_hub_download(repo_id="maxin-cn/Latte", filename="t2v.pt", revision="main")
-print(f"t2v.pt downloaded to {t2v_path}")
+# Define the repository and filename
+repo_id = "LanguageBind/Open-Sora-Plan-v1.0.0"
+filename = "vae/diffusion_pytorch_model.safetensors"
 
-# Verify t2v.pt download
-if os.path.exists(t2v_path):
-    print("t2v.pt downloaded successfully.")
-else:
-    print("Failed to download t2v.pt.")
+# Download the file
+file_path = hf_hub_download(repo_id=repo_id, filename=filename)
 
-# Download t2v_v20240523.pt
-t2v_v20240523_path = hf_hub_download(repo_id="maxin-cn/Latte", filename="t2v_v20240523.pt", revision="main")
-print(f"t2v_v20240523.pt downloaded to {t2v_v20240523_path}")
-
-# Verify t2v_v20240523.pt download
-if os.path.exists(t2v_v20240523_path):
-    print("t2v_v20240523.pt downloaded successfully.")
-else:
-    print("Failed to download t2v_v20240523.pt.")
+print(f"File downloaded to {file_path}")
