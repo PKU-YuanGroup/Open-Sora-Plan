4387a69157c8aba7916c6ca6ee7a465798e2bc54
Eric Sheen <eric.sprog@gmail.com>
2024-05-23 17:13:05 -0700

    change ae path

--- scripts/download_vae.py
+++ scripts/download_vae.py
@@ -11,3 +11,6 @@ files = ["config.json", "diffusion_pytorch_model.safetensors"]
 for file_name in files:
     file_path = hf_hub_download(repo_id=repo_id, subfolder=subfolder, filename=file_name)
     print(f"{file_name} downloaded to {file_path}")
+
+# mkdir vae
+# cp /root/.cache/huggingface/hub/models--LanguageBind--Open-Sora-Plan-v1.0.0/snapshots/92a849c00ab99ae2c7cd7ff156f2ae02eb25c45d/vae/* vae
