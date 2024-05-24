5de82a4ce78fc25551150a9006a985f4df47fa62
Eric Sheen <eric.sprog@gmail.com>
2024-05-23 17:06:21 -0700

    download vae script

---
+++ scripts/download_vae.py
@@ -0,0 +1,13 @@
+from huggingface_hub import hf_hub_download
+
+# Specify the repository and the directory to download
+repo_id = "LanguageBind/Open-Sora-Plan-v1.0.0"
+subfolder = "vae"
+
+# Files to download
+files = ["config.json", "diffusion_pytorch_model.safetensors"]
+
+# Loop to download each file
+for file_name in files:
+    file_path = hf_hub_download(repo_id=repo_id, subfolder=subfolder, filename=file_name)
+    print(f"{file_name} downloaded to {file_path}")
