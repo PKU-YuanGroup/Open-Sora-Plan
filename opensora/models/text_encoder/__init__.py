2a2c52e747d76ade2643e39cee287c2861de4885
LinB203 <2267330597@qq.com>
2024-06-12 19:27:46 +0800

    fix random video

--- opensora/models/text_encoder/__init__.py
+++ opensora/models/text_encoder/__init__.py
@@ -9,8 +9,8 @@ class T5Wrapper(nn.Module):
     def __init__(self, args, **kwargs):
         super(T5Wrapper, self).__init__()
         self.model_name = args.text_encoder_name
-        # self.text_enc = T5EncoderModel.from_pretrained(self.model_name, cache_dir=args.cache_dir, **kwargs).eval()
-        self.text_enc = T5EncoderModel.from_pretrained(r'/storage/ongoing/new/Open-Sora-Plan/cache_dir/models--DeepFloyd--t5-v1_1-xxl/snapshots/c9c625d2ec93667ec579ede125fd3811d1f81d37', cache_dir=args.cache_dir, **kwargs).eval()
+        self.text_enc = T5EncoderModel.from_pretrained(self.model_name, cache_dir=args.cache_dir, **kwargs).eval()
+        # self.text_enc = T5EncoderModel.from_pretrained(r'/storage/ongoing/new/Open-Sora-Plan/cache_dir/models--DeepFloyd--t5-v1_1-xxl/snapshots/c9c625d2ec93667ec579ede125fd3811d1f81d37', cache_dir=args.cache_dir, **kwargs).eval()
 
     def forward(self, input_ids, attention_mask):
         text_encoder_embs = self.text_enc(input_ids=input_ids, attention_mask=attention_mask)['last_hidden_state']
