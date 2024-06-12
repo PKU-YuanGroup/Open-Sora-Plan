fix precision-problem of deepspeed on npu

opensora/adaptor/stage_1_and_2.py
@@ -5,6 +5,7 @@
 
 import torch
 import os
+import pdb
 from deepspeed import comm as dist
 from packaging import version as pkg_version
 from collections import OrderedDict
@@ -274,7 +275,6 @@ class DeepSpeedZeroOptimizer(ZeROOptimizer):
 
         # align nccl all-gather send buffers to 4-byte boundary
         self.nccl_start_alignment_factor = 512  # 4-byte alignment/sizeof(fp16) = 2
-        print("Set nccl_start_alignment_factor = 52...")
 
         assert (
             allgather_bucket_size % self.nccl_start_alignment_factor == 0
@@ -1447,26 +1447,23 @@ class DeepSpeedZeroOptimizer(ZeROOptimizer):
 
         tensor_to_allreduce = tensor
 
-        # if pg_correctness_test or self.sequence_parallel_size > 1:
-        #     communication_data_type = torch.float32
-        # else:
-        #     communication_data_type = self.communication_data_type
-        communication_data_type = torch.float32
+        if pg_correctness_test or self.sequence_parallel_size > 1:
+            communication_data_type = torch.float32
+        else:
+            communication_data_type = self.communication_data_type
+
         if communication_data_type != tensor.dtype:
             tensor_to_allreduce = tensor.to(communication_data_type)
 
-        # if divide:
-        #     tensor_to_allreduce.div_(dist.get_world_size(group=process_group) / float(self.sequence_parallel_size))
+        if divide:
+            tensor_to_allreduce.div_(dist.get_world_size(group=process_group) / float(self.sequence_parallel_size))
 
-        tensor_to_allreduce = tensor_to_allreduce.contiguous()
-        # print("call tensor_to_allreduce.contiguous() ....")
-
-        # if rank is None:
-        #     #    "All Reducing"
-        #     dist.all_reduce(tensor_to_allreduce, group=process_group)
-        # else:
-        #     global_rank = dist.get_global_rank(process_group, rank)
-        #     dist.reduce(tensor_to_allreduce, global_rank, group=process_group)
+        if rank is None:
+            #    "All Reducing"
+            dist.all_reduce(tensor_to_allreduce, group=process_group)
+        else:
+            global_rank = dist.get_global_rank(process_group, rank)
+            dist.reduce(tensor_to_allreduce, global_rank, group=process_group)
 
         if communication_data_type != tensor.dtype and tensor is not tensor_to_allreduce:
             if rank is None or rank == dist.get_rank(group=process_group):
@@ -1867,6 +1864,10 @@ class DeepSpeedZeroOptimizer(ZeROOptimizer):
             self.reset_cpu_buffers()
 
         self.timers(OPTIMIZER_ALLGATHER_TIMER).start()
+
+        # if dist.get_rank(group=self.dp_process_group) == 0:
+        #     pdb.set_trace()  # 或者使用其他调试工具
+
         # Gather the updated weights from everyone.
         # Then all partitions of the model parameters are updated and ready for next round forward.
         all_gather_dp_groups(groups_flat=self.bit16_groups_flat,

opensora/models/diffusion/opensora/modules.py
@@ -236,10 +236,9 @@ class PatchEmbed2D(nn.Module):
         video_latent, image_latent = latent[:, :num_frames], latent[:, num_frames:]
 
         # temp_pos_embed = temp_pos_embed.unsqueeze(2) * self.temp_embed_gate.tanh()
-        # temp_pos_embed = temp_pos_embed.unsqueeze(2)
-        # video_latent = (video_latent + temp_pos_embed).to(video_latent.dtype) if video_latent is not None and video_latent.numel() > 0 else None
-        # image_latent = (image_latent + temp_pos_embed[:, :1]).to(image_latent.dtype) if image_latent is not None and image_latent.numel() > 0 else None
-
+        temp_pos_embed = temp_pos_embed.unsqueeze(2)
+        video_latent = (video_latent + temp_pos_embed).to(video_latent.dtype) if video_latent is not None and video_latent.numel() > 0 else None
+        image_latent = (image_latent + temp_pos_embed[:, :1]).to(image_latent.dtype) if image_latent is not None and image_latent.numel() > 0 else None
 
         video_latent = rearrange(video_latent, 'b t n c -> b (t n) c') if video_latent is not None and video_latent.numel() > 0 else None
         image_latent = rearrange(image_latent, 'b t n c -> (b t) n c') if image_latent is not None and image_latent.numel() > 0 else None

opensora/npu_config.py
@@ -16,6 +16,7 @@ except:
     npu_is_available = False
 
 from contextlib import contextmanager
+import types
 # from opensora.acceleration.parallel_states import enable_LCCL, hccl_info, lccl_info
 
 
@@ -78,19 +79,21 @@ class NPUConfig:
         self.mm = dict()
 
         if self.on_npu:
+            import deepspeed
+            import sys
             torch_npu.npu.set_compile_mode(jit_compile=False)
-            import deepspeed.runtime.bf16_optimizer as optimizer
-            from opensora.adaptor.bf16_optimizer import BF16_Optimizer
-            optimizer.BF16_Optimizer.__init__ = BF16_Optimizer.__init__
-
-            import deepspeed.runtime.zero.stage_1_and_2 as stage_1_and_2
-            from opensora.adaptor.stage_1_and_2 import DeepSpeedZeroOptimizer
-            stage_1_and_2.DeepSpeedZeroOptimizer.__init__ = DeepSpeedZeroOptimizer.__init__
-            stage_1_and_2.DeepSpeedZeroOptimizer.allreduce_bucket = DeepSpeedZeroOptimizer.allreduce_bucket
 
             import deepspeed.runtime.utils as utils
-            from opensora.adaptor.utils import all_gather_into_tensor_dp_groups
-            utils.all_gather_into_tensor_dp_groups = all_gather_into_tensor_dp_groups
+            from opensora.adaptor.utils import all_gather_dp_groups, all_gather_into_tensor_dp_groups
+            utils.all_gather_dp_groups = all_gather_dp_groups
+
+            import deepspeed.runtime.bf16_optimizer as bf16_optimizer
+            from opensora.adaptor.bf16_optimizer import BF16_Optimizer
+            self.replace_methods(bf16_optimizer.BF16_Optimizer, BF16_Optimizer)
+
+            from opensora.adaptor.stage_1_and_2 import DeepSpeedZeroOptimizer
+            import deepspeed.runtime.zero.stage_1_and_2 as stage_1_and_2
+            self.replace_methods(stage_1_and_2.DeepSpeedZeroOptimizer, DeepSpeedZeroOptimizer, ['_has_inf_or_nan'])
 
         if "RANK" in os.environ:
             self.rank = int(os.environ["RANK"])
@@ -100,6 +103,15 @@ class NPUConfig:
             self.rank = torch.cuda.current_device()
             self.world_size = self.N_NPU_PER_NODE
         self.print_with_rank(f"The npu_config.on_npu is {self.on_npu}")
+
+    def replace_methods(self, target_class, source_class, skip_fcns=[]):
+        for attr_name in dir(source_class):
+            attr_value = getattr(source_class, attr_name)
+            if isinstance(attr_value, (staticmethod, classmethod)) or attr_name in skip_fcns:
+                print(f"skip replace {attr_name}")
+                continue
+            elif isinstance(attr_value, types.FunctionType):
+                setattr(target_class, attr_name, attr_value)
 
     def get_attention_mask(self, attention_mask, repeat_num):
         if self.on_npu and attention_mask is not None: