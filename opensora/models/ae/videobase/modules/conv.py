fix precision problem for vae decode when using npu

opensora/models/ae/videobase/modules/conv.py
@@ -101,11 +101,10 @@ class CausalConv3d(nn.Module):
     def forward(self, x):
         if npu_config is not None and npu_config.on_npu:
             x_dtype = x.dtype
-            x = x.to(npu_config.replaced_type)
-            n, c, d, h, w = x.shape
-            x = x.reshape(n * c, d, h * w)
-            x = self.pad(x)
-            x = x.reshape(n, c, -1, h, w)
+            first_frame_pad = x[:, :, :1, :, :].repeat(
+                (1, 1, self.time_kernel_size - 1, 1, 1)
+            )  # b c t h w
+            x = torch.concatenate((first_frame_pad, x), dim=2)  # 3 + 16
             return npu_config.run_conv3d(self.conv, x, x_dtype)
         else:
             # 1 + 16   16 as video, 1 as image

opensora/npu_config.py
@@ -71,6 +71,7 @@ class NPUConfig:
         self.original_run_dtype = None
         self.zp_manager = zp_manager
         self.replaced_type = torch.float32
+        self.conv_dtype = torch.float16
         if self.enable_FA and self.enable_FP32:
             self.inf_float = -10000.0
         else:
@@ -252,7 +253,7 @@ class NPUConfig:
                 out_dtype = x.dtype
 
             with torch.cuda.amp.autocast(enabled=False):
-                x = operator.to(tmp_dtype)(x.to(tmp_dtype))
+                x = operator.to(device=x.device, dtype=tmp_dtype)(x.to(tmp_dtype))
                 x = x.to(out_dtype)
                 if out_nd_format:
                     return self.npu_format_cast(x)
@@ -289,7 +290,7 @@ class NPUConfig:
             f"Median: {median_val}, Std: {std_val}, Shape: {shape}, Type: {x_dtype}")
 
     def run_conv3d(self, operator, x, out_dtype):
-        return self._run(operator, x, torch.float16, out_dtype, out_nd_format=True)
+        return self._run(operator, x, self.conv_dtype, out_dtype, out_nd_format=True)
 
     def run_pool_2d(self, operator, x):
         return self._run(operator, x, self.replaced_type)