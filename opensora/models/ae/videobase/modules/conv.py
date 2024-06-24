fix mix-precision with deepspeed zero mode

opensora/models/ae/videobase/modules/conv.py
@@ -101,7 +101,7 @@ class CausalConv3d(nn.Module):
     def forward(self, x):
         if npu_config is not None and npu_config.on_npu:
             x_dtype = x.dtype
-            x = x.to(torch.float16)
+            x = x.to(npu_config.replaced_type)
             n, c, d, h, w = x.shape
             x = x.reshape(n * c, d, h * w)
             x = self.pad(x)

opensora/models/diffusion/opensora/modules.py
@@ -828,12 +828,12 @@ class FeedForward_Conv3d(nn.Module):
             x = self.project_out(out)
         else:
             x_dtype = x.dtype
-            x = npu_config.run_conv3d(self.project_in, x, torch.float16)
+            x = npu_config.run_conv3d(self.project_in, x, npu_config.replaced_type)
             x = rearrange(x, 'b (t h w) d -> b d t h w', t=t, h=h, w=w)
             x = F.gelu(x)
             out = x
             for module in self.dwconv:
-                out = out + npu_config.run_conv3d(module, x, torch.float16)
+                out = out + npu_config.run_conv3d(module, x, npu_config.replaced_type)
             out = rearrange(out, 'b d t h w -> b (t h w) d', t=t, h=h, w=w)
             x = npu_config.run_conv3d(self.project_out, out, x_dtype)
         return x

opensora/npu_config.py
@@ -8,6 +8,7 @@ import torch
 import subprocess
 import sys
 import threading
+import gc
 import torch.distributed as dist
 
 from opensora.adaptor.zp_manager import zp_manager
@@ -69,6 +70,7 @@ class NPUConfig:
         self.current_run_dtype = None
         self.original_run_dtype = None
         self.zp_manager = zp_manager
+        self.replaced_type = torch.float32
         if self.enable_FA and self.enable_FP32:
             self.inf_float = -10000.0
         else:
@@ -112,6 +114,7 @@ class NPUConfig:
             self.world_size = self.N_NPU_PER_NODE
         self.print_with_rank(f"The npu_config.on_npu is {self.on_npu}")
         self.bind_thread_to_cpu()
+        gc.set_threshold(700, 10, 10000)
 
     def get_total_cores(self):
         try:
@@ -261,6 +264,9 @@ class NPUConfig:
     def run_group_norm(self, operator, x):
         return self._run(operator, x, torch.float32)
 
+    def run_layer_norm(self, operator, x):
+        return self._run(operator, x, torch.float32)
+
     def print_tensor_stats(self, tensor, name="Tensor", rank=None):
         if rank and rank != self.rank:
             return
@@ -286,12 +292,12 @@ class NPUConfig:
         return self._run(operator, x, torch.float16, out_dtype, out_nd_format=True)
 
     def run_pool_2d(self, operator, x):
-        return self._run(operator, x, torch.float16)
+        return self._run(operator, x, self.replaced_type)
 
     def run_pad_2d(self, operator, x, pad, mode="constant"):
         if self.on_npu:
             x_dtype = x.dtype
-            x = x.to(torch.float16)
+            x = x.to(self.replaced_type)
             x = operator(x, pad, mode)
             x = x.to(x_dtype)
         else:

opensora/sample/pipeline_opensora.py
@@ -34,6 +34,10 @@ from diffusers.utils import (
 from diffusers.utils.torch_utils import randn_tensor
 from diffusers.pipelines.pipeline_utils import DiffusionPipeline, ImagePipelineOutput
 
+try:
+    from opensora.npu_config import npu_config
+except:
+    npu_config = None
 
 logger = logging.get_logger(__name__)  # pylint: disable=invalid-name
 
@@ -784,6 +788,8 @@ class OpenSoraPipeline(DiffusionPipeline):
 
                 # compute previous image: x_t -> x_t-1
                 latents = self.scheduler.step(noise_pred, t, latents, **extra_step_kwargs, return_dict=False)[0]
+                # npu_config.print_tensor_stats(latents, f"latents_i{i}_t{t}", rank=0)
+                assert not torch.isnan(latents).any().item(), "latents contains NaN values"
 
                 # call the callback, if provided
                 if i == len(timesteps) - 1 or ((i + 1) > num_warmup_steps and (i + 1) % self.scheduler.order == 0):
@@ -807,10 +813,14 @@ class OpenSoraPipeline(DiffusionPipeline):
             return (image,)
 
         return ImagePipelineOutput(images=image)
-    
-    
+
+
     def decode_latents(self, latents):
+        if npu_config:
+            npu_config.print_tensor_stats(latents, f"before vae", rank=0)
         video = self.vae.decode(latents.to(self.vae.vae.dtype))
+        if npu_config:
+            npu_config.print_tensor_stats(video, f"after vae, vae.dtype={self.vae.vae.dtype}", rank=0)
         video = ((video / 2.0 + 0.5).clamp(0, 1) * 255).to(dtype=torch.uint8).cpu().permute(0, 1, 3, 4, 2).contiguous() # b t h w c
         # we always cast to float32 as this does not cause significant overhead and is compatible with bfloa16
         return video

opensora/sample/sample_t2v_on_npu.py
@@ -17,6 +17,7 @@ from transformers import T5EncoderModel, T5Tokenizer, AutoTokenizer
 
 import os, sys
 
+from opensora.adaptor.modules import replace_with_fp32_forwards
 from opensora.models.ae import ae_stride_config, getae, getae_wrapper
 from opensora.models.ae.videobase import CausalVQVAEModelWrapper, CausalVAEModelWrapper
 from opensora.models.diffusion.udit.modeling_udit import UDiTT2V
@@ -167,6 +168,7 @@ if __name__ == "__main__":
     args = parser.parse_args()
 
     npu_config.print_msg(args)
+    replace_with_fp32_forwards()
 
     # 初始化分布式环境
     local_rank = int(os.getenv('RANK', 0))
@@ -176,7 +178,7 @@ if __name__ == "__main__":
     dist.init_process_group(backend='hccl', init_method='env://', world_size=8, rank=local_rank)
 
     # torch.manual_seed(args.seed)
-    weight_dtype = torch.float16
+    weight_dtype = torch.float32
     device = torch.cuda.current_device()
 
     # vae = getae_wrapper(args.ae)(args.model_path, subfolder="vae", cache_dir=args.cache_dir)
@@ -227,13 +229,18 @@ if __name__ == "__main__":
 
     latest_path = None
     save_img_path = args.save_img_path
+    first_in = False
     while True:
         cur_path = get_latest_path()
         if cur_path == latest_path:
-            time.sleep(5)
+            time.sleep(60)
             continue
 
-        time.sleep(1)
+        if not first_in:
+            first_in = True
+        else:
+            time.sleep(1200)
+
         latest_path = cur_path
         npu_config.print_msg(f"The latest_path is {latest_path}")
         full_path = f"{args.model_path}/{latest_path}/model_ema"