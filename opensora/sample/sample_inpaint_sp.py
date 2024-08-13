e61e132e0d7e3a7634a8e4231d48927b97c03348
yunyang Ge <134182944+yunyangge@users.noreply.github.com>
2024-08-13 20:36:45 +0800

    Update sample_inpaint_sp.py

--- opensora/sample/sample_inpaint_sp.py
+++ opensora/sample/sample_inpaint_sp.py
@@ -273,15 +273,9 @@ if __name__ == "__main__":
 
     vae.vae_scale_factor = ae_stride_config[args.ae]
 
-    text_encoder = MT5EncoderModel.from_pretrained("/storage/ongoing/new/Open-Sora-Plan/cache_dir/mt5-xxl", 
-                                                   cache_dir=args.cache_dir, low_cpu_mem_usage=True, 
-                                                   torch_dtype=weight_dtype).to(device)
-    tokenizer = AutoTokenizer.from_pretrained("/storage/ongoing/new/Open-Sora-Plan/cache_dir/mt5-xxl", 
-                                              cache_dir=args.cache_dir)
-    
-    # text_encoder = MT5EncoderModel.from_pretrained(args.text_encoder_name, cache_dir=args.cache_dir,
-    #                                               low_cpu_mem_usage=True, torch_dtype=weight_dtype).to(device)
-    # tokenizer = T5Tokenizer.from_pretrained(args.text_encoder_name, cache_dir=args.cache_dir)
+    text_encoder = MT5EncoderModel.from_pretrained(args.text_encoder_name, cache_dir=args.cache_dir,
+                                                  low_cpu_mem_usage=True, torch_dtype=weight_dtype).to(device)
+    tokenizer = T5Tokenizer.from_pretrained(args.text_encoder_name, cache_dir=args.cache_dir)
 
     # set eval mode
     vae.eval()
