6fb441f737453b43cde869863cac17d8b137d194
LinB203 <2267330597@qq.com>
2024-10-05 23:28:14 +0800

    1.3 train

--- scripts/causalvae/rec_video.sh
+++ scripts/causalvae/rec_video.sh
@@ -2,7 +2,7 @@ CUDA_VISIBLE_DEVICES=1 python examples/rec_video.py \
     --ae WFVAEModel_D8_4x8x8 \
     --ae_path "/storage/lcm/WF-VAE/results/latent8" \
     --video_path /storage/lcm/WF-VAE/testvideo/gm1190263332-337350271.mp4 \
-    --rec_path rec_tile.mp4 \
+    --rec_path rec_tile_.mp4 \
     --device cuda \
     --sample_rate 1 \
     --num_frames 61 \
