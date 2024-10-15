c71989341a46484e208cb2cbaeba0401a2a89a1a
yunyangge <gyy23233.163.com>
2024-10-15 21:58:27 +0800

    inpaint 1.3

--- scripts/text_condition/npu/sample_inpaint_v1_3.sh
+++ scripts/text_condition/npu/sample_inpaint_v1_3.sh
@@ -3,7 +3,7 @@ export TASK_QUEUE_ENABLE=0
 torchrun --nnodes=1 --nproc_per_node 8 --master_port 29522 \
     -m opensora.sample.sample \
     --model_type "inpaint" \
-    --model_path /home/save_dir/runs/i2v_1_3_hq_finetune/checkpoint-20000/model_ema \
+    --model_path /home/save_dir/runs/i2v_1_3_hq_finetune/checkpoint-7000/model_ema \
     --version v1_3 \
     --num_frames 93 \
     --max_hxw 236544 \
@@ -13,13 +13,13 @@ torchrun --nnodes=1 --nproc_per_node 8 --master_port 29522 \
     --conditional_pixel_values_path /home/image_data/gyy/mmdit/Open-Sora-Plan/validation_dir/cond_imgs_path.txt \
     --ae WFVAEModel_D8_4x8x8 \
     --ae_path "/home/save_dir/lzj/formal_8dim/latent8" \
-    --save_img_path "./test_inpaint_v1_3_final" \
+    --save_img_path "./test_inpaint_v1_3_sucai" \
     --fps 18 \
     --guidance_scale 7.5 \
     --num_sampling_steps 50 \
     --max_sequence_length 512 \
     --sample_method EulerAncestralDiscrete \
-    --seed 1234 \
+    --seed 2514 \
     --num_samples_per_prompt 1 \
     --prediction_type "v_prediction" \
     --crop_for_hw \
