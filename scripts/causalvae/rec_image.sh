ad6a1f61afceca8092763c8eb9766cb8c22dda78
LinB203 <2267330597@qq.com>
2024-10-16 00:26:20 +0800

    check vae

--- scripts/causalvae/rec_image.sh
+++ scripts/causalvae/rec_image.sh
@@ -2,6 +2,6 @@ CUDA_VISIBLE_DEVICES=0 python examples/rec_image.py \
     --ae WFVAEModel_D32_8x8x8 \
     --ae_path "/storage/lcm/WF-VAE/results/new888" \
     --image_path /storage/dataset/image/anytext3m/ocr_data/Art/images/gt_5544.jpg \
-    --rec_path rec.jpg \
+    --rec_path rec_.jpg \
     --device cuda \
     --short_size 512 
