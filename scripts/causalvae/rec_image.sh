9a048c56dbf1407f37f39835ce34a6070abf959d
LinB203 <2267330597@qq.com>
2024-10-19 16:33:11 +0800

    check vae888

--- scripts/causalvae/rec_image.sh
+++ scripts/causalvae/rec_image.sh
@@ -1,5 +1,5 @@
 CUDA_VISIBLE_DEVICES=0 python examples/rec_image.py \
-    --ae WFVAEModel_D8_4x8x8 \
+    --ae WFVAEModel_D32_8x8x8 \
     --ae_path "/storage/lcm/WF-VAE/results/Middle888" \
     --image_path /storage/dataset/image/anytext3m/ocr_data/Art/images/gt_5544.jpg \
     --rec_path rec_.jpg \
