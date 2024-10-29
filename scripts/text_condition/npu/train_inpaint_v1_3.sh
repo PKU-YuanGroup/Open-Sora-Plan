f931c83003128ac475fd6d70d8dabd611bfb3239
yunyang Ge <134182944+yunyangge@users.noreply.github.com>
2024-10-29 17:02:06 +0800

    Update train_inpaint_v1_3.sh

--- scripts/text_condition/npu/train_inpaint_v1_3.sh
+++ scripts/text_condition/npu/train_inpaint_v1_3.sh
@@ -21,7 +21,7 @@ accelerate launch \
     --text_encoder_name_1 google/mt5-xxl \
     --cache_dir "../../cache_dir/" \
     --dataset inpaint \
-    --data "scripts/train_data/video_data_debug_on_npu.txt" \
+    --data "scripts/train_data/video_data.txt" \
     --ae WFVAEModel_D8_4x8x8 \
     --ae_path "/home/save_dir/lzj/formal_8dim/latent8" \
     --vae_fp32 \
@@ -64,8 +64,8 @@ accelerate launch \
     --mask_config scripts/train_configs/mask_config.yaml \
     --add_noise_to_condition \
     --default_text_ratio 0.5 \
-    --resume_from_checkpoint="latest" \
-    # --pretrained_transformer_model_path /home/save_dir/pretrained/i2v_ckpt14777_ema \
+    --resume_from_checkpoint="latest" 
+    # --pretrained "/home/save_dir/pretrained/93x640x640_144k_ema" 
     # --force_resolution
     # --force_resolution \
     # --max_height 352 \
