3ba6aafe82c49d6418ff7682b4edc61bd00da149
He Xianyi <79952506+pkuhxy@users.noreply.github.com>
2024-12-27 17:03:12 +0800

    Dpgbench (#561)
    * Dpgbench
    
    * debug
    
    * 1219 dpgbench
    
    * dpgbench 1220
    
    * 1221 dpgbench
    
    * 1222 dpgbench
    
    ---------
    
    Co-authored-by: LinB203 <2267330597@qq.com>

---
+++ opensora/eval/dpgbench/step2_compute_dpg_bench.sh
@@ -0,0 +1,17 @@
+IMAGE_ROOT_PATH=$1
+RESOLUTION=$2
+PIC_NUM=${PIC_NUM:-4}
+PROCESSES=${PROCESSES:-8}
+PORT=${PORT:-1234}
+RESULTPATH="/storage/hxy/t2i/results/dpgbench/results/mmdit_last_results.txt" #保存结果的txt路径
+VQACKPT="/storage/hxy/t2i/weight/dpgbench" #保存vqamodel的本地权重路径
+
+accelerate launch --num_machines 1 --num_processes $PROCESSES --multi_gpu --mixed_precision "fp16" --main_process_port $PORT \
+  step2_compute_dpg_bench.py \
+  --image_root_path $IMAGE_ROOT_PATH \
+  --resolution $RESOLUTION \
+  --pic_num $PIC_NUM \
+  --res_path $RESULTPATH \
+  --vqa_model mplug \
+  # --vqa_model_ckpt $VQACKPT \ 
+
