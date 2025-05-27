afab08a5e0540164a9e04d7b91a9f75497a29eee
yunyangge <gyy23233.163.com>
2025-05-27 19:54:53 +0800

    clean code

--- pretrain_sora.py
+++ pretrain_sora.py
@@ -64,9 +64,6 @@ def loss_func(output_tensor):
     """Loss function."""
     loss = output_tensor.mean()
     max_loss = get_max_loss_across_data_parallel_group([loss])
-    # loss_clone = loss.clone().detach()
-    # loss_clone = (1.0 - AdaptiveGradClipInfo.zero_grad_flag) * loss_clone / (AdaptiveGradClipInfo.clip_coef + 1e-15)
-    # avg_loss = average_losses_across_data_parallel_group([loss_clone])
     avg_loss = average_losses_across_data_parallel_group([loss])
     loss = loss.unsqueeze(0)
     return loss, {"avg_loss": avg_loss[0], "max_loss": max_loss[0]}
@@ -86,9 +83,7 @@ def get_batch_for_step(data_iterator):
 
 def forward_step(data_iterator, model):
     """Forward step."""
-    torch.distributed.barrier()
     batch = get_batch_for_step(data_iterator)
-    torch.distributed.barrier()
     video = batch.pop(VIDEO, None)
     prompt_ids = batch.pop(PROMPT_IDS, None)
     video_mask = batch.pop(VIDEO_MASK, None)
@@ -104,9 +99,7 @@ def forward_step(data_iterator, model):
         prompt_mask_2=prompt_mask_2,
         **batch,
     )
-    torch.distributed.barrier()
     loss_dict = unwrap_model(model).compute_loss(*output_tensor_list)
-    torch.distributed.barrier()
     return loss_dict, loss_func
 
 # pretrain函数调用datasets_provider, 而pretrain中dataloader_type传external，所以这里返回的iter就是实际用到的iter
@@ -134,60 +127,13 @@ def train_valid_test_datasets_provider(train_val_test_num_samples):
         process_group=process_group,
     )
     data_iterator, _, _ = build_iterations(train_dl=train_dataloader, iterator_type='single')
-    torch.distributed.barrier()
     return data_iterator, None, None
 
 
 if __name__ == "__main__":
     train_valid_test_datasets_provider.is_distributed = True
 
-    data_meta_info_list = [
-        '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/test_data.txt',
-        # '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/test_data.txt',
-        '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data00.txt',
-        '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data01.txt',
-        '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data02.txt',
-        '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data03.txt',
-        '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data04.txt',
-        '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data05.txt',
-        '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data06.txt',
-        '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data07.txt',
-        '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data08.txt',
-        '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data09.txt',
-        # '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data10.txt',
-        # '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data11.txt',
-        # '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data12.txt',
-        # '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data13.txt',
-        # '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data14.txt',
-        # '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data15.txt',
-        # '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data16.txt',
-        # '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data17.txt',
-        # '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data18.txt',
-        # '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data19.txt',
-        # '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data20.txt',
-        # '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data21.txt',
-        # '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data22.txt',
-        # '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data23.txt',
-        # '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data24.txt',
-        # '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data25.txt',
-        # '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data26.txt',
-        # '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data27.txt',
-        # '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data28.txt',
-        # '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data29.txt',
-        # '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data30.txt',
-        # '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data31.txt',
-        # '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data32.txt',
-        # '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data33.txt',
-        # '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data34.txt',
-        # '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data35.txt',
-        # '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data36.txt',
-        # '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data37.txt',
-        # '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data38.txt',
-        # '/work/share/projects/gyy/mindspeed/Open-Sora-Plan/examples/opensoraplan1.5/data39.txt',
-    ]
-
     pretrain(
-        data_meta_info_list,
         train_valid_test_datasets_provider,
         model_provider,
         ModelType.encoder_or_decoder,
@@ -195,7 +141,4 @@ if __name__ == "__main__":
         extra_args_provider=mm_extra_args_provider,
         args_defaults={"dataloader_type": "external", "vision_pretraining": False},
     )
-    print(f"pretrain done, rank = {torch.distributed.get_rank()}, wait for all processes to exit...")
-    torch.distributed.barrier()
-    print(f"all processes exit, rank = {torch.distributed.get_rank()}")
     sys.exit(0)
