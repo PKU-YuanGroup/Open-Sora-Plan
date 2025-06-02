78e78b37bc0f724a4d3de32b3f894c79f29bd880
yunyangge <gyy23233@163.com>
2025-06-02 16:13:35 +0800

    clean files

--- rdzv_run_test.sh
+++
@@ -1,8 +0,0 @@
-pkill -9 pt_main_thread
-pkill -9 python
-# export PROJECT_NAME="57x288x512_node64_tp2_bs2_gc2_lr2e-5_1e-6_mwmode_fp32"
-export PROJECT_NAME="test_8_0_1_autocast"
-export PROJECT_EXP_NAME="all_data"
-export PROJECT_DIR="/work/share1/checkpoint/gyy/osp/$PROJECT_NAME"
-
-bash examples/opensoraplan1.5/pretrain_opensoraplan1_5_rdzv.sh
--- test_ckpt.sh
+++
@@ -1,7 +0,0 @@
-MINDSPEED_PATH="./MindSpeed/"
-export PYTHONPATH=${MINDSPEED_PATH}:$PYTHONPATH
-
-export LD_PRELOAD=/lib/aarch64-linux-gnu/libGLdispatch.so.0
-export LD_PRELOAD=/lib/aarch64-linux-gnu/libgomp.so.1:$LD_PRELOAD
-
-python convert_ckpt_from_megatron_to_pt.py
